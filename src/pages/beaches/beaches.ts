835fbebd27ecaaf00799edb5c2110a1db3b915eb
Kjwolf78 <kjwolf78@gmail.com>
2017-05-10 17:28:20 -0400

    Got it all but beaches.ts is still tricky

--- src/pages/beaches/beaches.ts
+++ src/pages/beaches/beaches.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { LocationProvider } from '../../providers/location-provider';
 //import LocationProvider here. Look at home.ts for an example.
 
 @Component({
@@ -7,13 +8,16 @@ import { NavController, NavParams } from 'ionic-angular';
   templateUrl: 'beaches.html'
 })
 export class BeachesPage {
+  slides=[];
   //add slides variable here like in home.ts but it won't be called locations. Call is slides.
 
-  constructor(public navCtrl: NavController, public navParams: NavParams /*add locationProvider here like we do in home.ts*/) {
-
-    //define slides variable here. Look to home.ts as reference but it won't be equal to locationProvider.locations
+  constructor(public navCtrl: NavController,private locationProvider:LocationProvider) {
+    this.slides=beaches.beachesSlides;
   
-  }
+} /*add locationProvider here like we do in home.ts*/
+
+//define slides variable here. Look to home.ts as reference but it won't be equal to locationProvider.locations
+
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad BeachesPage');
--- src/providers/location-provider.ts
+++ src/providers/location-provider.ts
@@ -2,16 +2,19 @@ import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 import * as home from '../assets/data/home.json';
+import * as beaches from '../assest/data/beaches.json';
 //import beaches.json here. Look at the line above. Instead of naming it "home", name it beaches.
 
 @Injectable()
 export class LocationProvider {
   
   locations=[];
+  beachesSlides=[];
   //add variable called beachesSlides here. It should look like the line above but not called locations.
 
   constructor(public http: Http) {
     this.locations = home.locations;
+    this.beachesSlides = beaches.beachesSlides;
     //define beachesSlides variable to beaches.json data here. Look at the line above to see how we set the locations variable to equal the data.locations
   }
 

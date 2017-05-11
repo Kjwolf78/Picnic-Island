703a781813c468983c9422f54ef5e2cf004c778e
Kjwolf78 <kjwolf78@gmail.com>
2017-05-11 16:54:29 -0400

    updates

--- src/providers/location-provider.ts
+++ src/providers/location-provider.ts
@@ -3,6 +3,8 @@ import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 import * as home from '../assets/data/home.json';
 import * as beaches from '../assets/data/beaches.json';
+import * as boatramp from '../assets/data/boatramp.json';
+
 //import beaches.json here. Look at the line above. Instead of naming it "home", name it beaches.
 
 @Injectable()
@@ -10,11 +12,15 @@ export class LocationProvider {
   
   locations=[];
   beachesSlides=[];
+  boatrampSlides=[];
+
   //add variable called beachesSlides here. It should look like the line above but not called locations.
 
   constructor(public http: Http) {
     this.locations = home.locations;
     this.beachesSlides = beaches.slides;
+    this.boatrampSlides = boatramp.slides;
+    
     //define beachesSlides variable to beaches.json data here. Look at the line above to see how we set the locations variable to equal the data.locations
   }
 
